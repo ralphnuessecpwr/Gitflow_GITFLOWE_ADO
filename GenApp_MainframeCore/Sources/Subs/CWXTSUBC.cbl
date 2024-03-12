97e2d5a7c2d639120a0575d38eda586e279dcc41
Ralph <ralph_nuesse@bmc.com>
2024-03-12 11:55:22 +0100

    Test Sonar Parms

--- GenApp_MainframeCore/Sources/Subs/CWXTSUBC.cbl
+++ GenApp_MainframeCore/Sources/Subs/CWXTSUBC.cbl
@@ -10,7 +10,7 @@
       *  CALCULATES COMMISIONS FOR SALES AND MANAGEMENT EMPLOYEE       *
       *  BASED ON MONTHLY SALES RATES                                  *
       *                                                                *
-      * DEMO 2024.03.12  - 11:42                                       *
+      * DEMO 2024.03.12  - 11:55                                       *
       *                                                                *
       ******************************************************************
        ENVIRONMENT DIVISION.
