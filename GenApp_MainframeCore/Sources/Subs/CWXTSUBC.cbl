18d834cc9c300b05e991e6dc5794e8f864c689fa
Ralph <ralph_nuesse@bmc.com>
2024-03-13 11:37:19 +0100

    Test Coco Extension

--- GenApp_MainframeCore/Sources/Subs/CWXTSUBC.cbl
+++ GenApp_MainframeCore/Sources/Subs/CWXTSUBC.cbl
@@ -10,7 +10,7 @@
       *  CALCULATES COMMISIONS FOR SALES AND MANAGEMENT EMPLOYEE       *
       *  BASED ON MONTHLY SALES RATES                                  *
       *                                                                *
-      * DEMO 2024.03.13  - 11:26                                       *
+      * DEMO 2024.03.13  - 11:37                                       *
       *                                                                *
       ******************************************************************
        ENVIRONMENT DIVISION.
