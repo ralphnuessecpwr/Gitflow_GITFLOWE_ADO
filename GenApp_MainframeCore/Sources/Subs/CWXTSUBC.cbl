2e02a62cbd7469bf66b80c2c0565e4c38b01780c
Ralph <ralph_nuesse@bmc.com>
2024-03-14 11:55:39 +0100

    Test new logic

--- GenApp_MainframeCore/Sources/Subs/CWXTSUBC.cbl
+++ GenApp_MainframeCore/Sources/Subs/CWXTSUBC.cbl
@@ -10,7 +10,7 @@
       *  CALCULATES COMMISIONS FOR SALES AND MANAGEMENT EMPLOYEE       *
       *  BASED ON MONTHLY SALES RATES                                  *
       *                                                                *
-      * DEMO 2024.03.14  - 11:37                                       *
+      * DEMO 2024.03.14  - 11:55                                       *
       *                                                                *
       ******************************************************************
        ENVIRONMENT DIVISION.
