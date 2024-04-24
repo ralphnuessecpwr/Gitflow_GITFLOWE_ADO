f6c37eaabce5f5b3f590f106cf8c6a745f5b7c32
Ralph <ralph_nuesse@bmc.com>
2024-04-24 09:02:02 +0200

    Demo

--- GenApp_MainframeCore/Sources/Subs/CWXTDATE.cbl
+++ GenApp_MainframeCore/Sources/Subs/CWXTDATE.cbl
@@ -7,7 +7,7 @@
       *  THIS CALLED PROGRAM IS PART OF THE COBOL DEMO PROGRAM USED    *
       *  FOR XPEDITER/TSO TRAINING SESSIONS                            *
       *                                                                *
-      * DEMO 2024.03.28  - 16:00                                       *
+      * DEMO 2024.04.24  - 09:00                                       *
       *                                                                *      
       ******************************************************************
        ENVIRONMENT DIVISION.
--- GenApp_MainframeCore/Sources/Subs/CWXTSUBC.cbl
+++ GenApp_MainframeCore/Sources/Subs/CWXTSUBC.cbl
@@ -10,7 +10,7 @@
       *  CALCULATES COMMISIONS FOR SALES AND MANAGEMENT EMPLOYEE       *
       *  BASED ON MONTHLY SALES RATES                                  *
       *                                                                *
-      * DEMO 2024.03.28  - 16:35                                       *
+      * DEMO 2024.04.24  - 09:00                                       *
       *                                                                *
       ******************************************************************
        ENVIRONMENT DIVISION.
